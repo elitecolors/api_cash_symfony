2140f8139600b6f0d1a06dfc4bbdc2a83d7ccc95
saidani <saidaniahmed125@gmail.com>
2022-03-12 15:37:11 +0100

    codeception test create user

--- symfony-app/tests/api/RegistrationCest.php
+++ symfony-app/tests/api/RegistrationCest.php
@@ -8,11 +8,33 @@ class RegistrationCest
 {
     public function _before(ApiTester $I)
     {
-        $I->sendPost('http://localhost:8080/auth/register');
+        $I->amGoingTo('create a new user');
+
     }
 
     // tests
     public function tryToTest(ApiTester $I)
     {
+        $I->sendPost('/auth/register', [
+            'role' => 'ROLE_ADMIN',
+            'email' => 'mailxx@mail.com',
+            'password' => 'test'
+        ]);
+
+        $I->expectTo('see 200 ok response');
+
+        $I->seeResponseCodeIs(200);
+
+
+        $I->amGoingTo('try with the same email');
+
+        $I->expectTo('see error');
+        $I->sendPost('/auth/register', [
+            'role' => 'ROLE_ADMIN',
+            'email' => 'mailxx@mail.com',
+            'password' => 'test'
+        ]);
+
+        $I->seeResponseCodeIs(403);
     }
 }
